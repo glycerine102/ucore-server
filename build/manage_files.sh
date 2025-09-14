e5548f43bd9837eca5718965e6cac2e42e7ee7fa
Nathan Dorsey <421420+glycerine102@users.noreply.github.com>
2025-09-13 20:03:32 -0400

    Ensure the Alacritty terminal info is set up

--- build/manage_files.sh
+++ build/manage_files.sh
@@ -15,4 +15,8 @@ mkdir /etc/netbird
 log "Load ZFS modules"
 echo zfs > /etc/modules-load.d/zfs.conf
 
+log "Ensure the Alacritty terminal info is set up"
+curl -s -L 'https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info' >alacritty.info
+tic -xe alacritty,alacritty-direct alacritty.info && rm alacritty.info
+
 log "Done managing files"
