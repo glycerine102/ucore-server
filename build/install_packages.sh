7437b7de739627b1bdf5c7ba2a5fb7e6a3f967f8
Nathan Dorsey <421420+glycerine102@users.noreply.github.com>
2025-09-18 13:45:58 -0400

    Apparently syncoid and sanoid are already installed

--- build/install_packages.sh
+++ build/install_packages.sh
@@ -8,17 +8,6 @@ log() {
   echo "=== $* ==="
 }
 
-log "Install sanoid package"
-
-SANOID_URL="https://github.com/decoyjoe/sanoid-portable/releases/download/2.2.0-2/sanoid-portable"
-SANOID_DIR="/usr/local/sbin"
-wget ${SANOID_URL} -O ${SANOID_DIR}/sanoid-portable
-chmod +x ${SANOID_DIR}/sanoid-portable
-sh ${SANOID_DIR}/sanoid-portable --assimilate
-ln -s ${SANOID_DIR}/sanoid-portable /usr/local/sbin/sanoid
-ln -s ${SANOID_DIR}/sanoid-portable /usr/local/sbin/syncoid
-ln -s ${SANOID_DIR}/sanoid-portable /usr/local/sbin/findoid
-
 log "Begin installing packages"
 
 system_packages=(
